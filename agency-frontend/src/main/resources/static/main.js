1fa40ce4a6c23932f2ace15c575f3888a95f85c3
Haytham Mohamed <haybu@hotmail.com>
2018-09-20 14:15:57 -0500

    update

--- agency-frontend/src/main/resources/static/main.js
+++ agency-frontend/src/main/resources/static/main.js
@@ -746,7 +746,7 @@ Object(_angular_platform_browser_dynamic__WEBPACK_IMPORTED_MODULE_1__["platformB
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
-module.exports = __webpack_require__(/*! /Users/hmohamed/github/agilehandy.io/RA-1-OAuth2/agency-ui/src/main.ts */"./src/main.ts");
+module.exports = __webpack_require__(/*! /Users/hmohamed/github/agilehandy.io/RA-OAuth2-Gen1/agency-ui/src/main.ts */"./src/main.ts");
 
 
 /***/ })
